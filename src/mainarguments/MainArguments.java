ad60caad664c84e04e2ab40d65f954aede116d3e
PhatBTSE61866 <PhatBTSE61866@192.168.1.70>
2016-11-14 19:04:51 +0700

    Ngu project-- First change

---
+++ src/mainarguments/MainArguments.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainarguments;
+
+/**
+ *
+ * @author PhatBTSE61866
+ */
+public class MainArguments {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OS provides " + args.length + " args for the " + "main function");
+        System.out.println("Here is the 1st arg:" + args[0]);
+        // TODO code application logic here
+    }
+    
+    //Java mainargument.MainArgument Le Dinh Tu Nguyen Cong Chinh
+    //OS provides 6 args for the main function
+    //Here is the 1st arg: le
+    
+}
